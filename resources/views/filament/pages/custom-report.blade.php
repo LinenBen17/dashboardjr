40bccfe1fbfbe8a9445ded34c88f00b4924e054d
LinenBen17 <aesquitecastilo@gmail.com>
2025-09-01 13:07:11 -0600

    Page Reporte elimnada

--- resources/views/filament/pages/custom-report.blade.php
+++
@@ -1,9 +0,0 @@
-<x-filament-panels::page>
-    <form action="">
-        {{ $this->form }}
-        <br>
-        <x-filament::button type="submit">
-            Submit
-        </x-filament::button>
-    </form>
-</x-filament-panels::page>
