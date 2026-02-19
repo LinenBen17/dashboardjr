def84f94dee80459a7396ab0ec26bc8c453ce9fa
LinenBen17 <aesquitecastilo@gmail.com>
2026-02-19 16:53:22 -0600

    Correccion de estructura en creacion de tabla

--- database/migrations/2025_06_17_112951_create_customers_table.php
+++ database/migrations/2025_06_17_112951_create_customers_table.php
@@ -24,7 +24,10 @@ return new class extends Migration
             $table->foreignId('departament_id')->constrained()->onDelete('cascade');
             $table->foreignId('town_id')->constrained()->onDelete('cascade');
             $table->string('prefix_origin', 10);
-            $table->foreignId('employee_id')->constrained()->onDelete('set null');
+            $table->foreignId('employee_id')
+                ->nullable()
+                ->constrained()
+                ->nullOnDelete();
             $table->timestamps();
         });
     }
