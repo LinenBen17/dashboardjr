51e0ad28c7962aee668782a48d5fcbcf39cc66c0
LinenBen17 <aesquitecastilo@gmail.com>
2026-02-21 00:56:18 -0600

    Configuracion para guardar imagenes en Storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/private'),
+            'root' => storage_path('storage'),
             'serve' => true,
             'throw' => false,
             'report' => false,
@@ -40,8 +40,8 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => storage_path('storage'),
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
             'report' => false,
