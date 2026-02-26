0e0b931ff7cfc86247b1ab0a09466405ccdf0a69
LinenBen17 <aesquitecastilo@gmail.com>
2026-02-26 01:04:49 -0600

    Reparacion EmployeeImporter

--- app/Filament/Imports/EmployeeImporter.php
+++ app/Filament/Imports/EmployeeImporter.php
@@ -16,104 +16,78 @@ class EmployeeImporter extends Importer
         return [
             ImportColumn::make('name')
                 ->requiredMapping()
-                ->rules(['required', 'max:25'])
                 ->fillRecordUsing(function (Employee $record, string $state): void {
                     $record->name = ucwords(strtolower($state));
                 }),
             ImportColumn::make('last_name')
                 ->requiredMapping()
-                ->rules(['required', 'max:25'])
                 ->fillRecordUsing(function (Employee $record, string $state): void {
                     $record->last_name = ucwords(strtolower($state));
                 }),
             ImportColumn::make('comments')
                 ->requiredMapping()
-                ->rules(['required'])
                 ->fillRecordUsing(function (Employee $record, string $state): void {
                     $record->comments = ucwords(strtolower($state));
                 }),
             ImportColumn::make('entry_date')
-                ->requiredMapping()
-                ->rules(['required', 'date']),
+                ->requiredMapping(),
             ImportColumn::make('id_agency')
-                ->numeric()
-                ->rules(['integer']),
+                ->numeric(),
             ImportColumn::make('id_charge')
-                ->numeric()
-                ->rules(['integer']),
+                ->numeric(),
             ImportColumn::make('bank_account')
                 ->requiredMapping()
-                ->numeric()
-                ->rules(['required', 'integer']),
+                ->numeric(),
             ImportColumn::make('address')
                 ->requiredMapping()
-                ->rules(['required', 'max:500'])
                 ->fillRecordUsing(function (Employee $record, string $state): void {
                     $record->address = ucwords(strtolower($state));
                 }),
             ImportColumn::make('town_id')
-                ->numeric()
-                ->rules(['integer']),
+                ->numeric(),
             ImportColumn::make('departament_id')
-                ->numeric()
-                ->rules(['integer']),
+                ->numeric(),
             ImportColumn::make('zone')
                 ->requiredMapping()
-                ->numeric()
-                ->rules(['required', 'integer']),
+                ->numeric(),
             ImportColumn::make('birthplace')
-                ->requiredMapping()
-                ->rules(['required', 'max:191']),
+                ->requiredMapping(),
             ImportColumn::make('phone')
-                ->requiredMapping()
-                ->rules(['required', 'max:15']),
+                ->requiredMapping(),
             ImportColumn::make('cellphone')
-                ->requiredMapping()
-                ->rules(['required', 'max:15']),
+                ->requiredMapping(),
             ImportColumn::make('birth_date')
-                ->requiredMapping()
-                ->rules(['required', 'date']),
+                ->requiredMapping(),
             ImportColumn::make('age')
                 ->requiredMapping()
-                ->numeric()
-                ->rules(['required', 'integer']),
+                ->numeric(),
             ImportColumn::make('civil_status_id')
-                ->numeric()
-                ->rules(['integer']),
+                ->numeric(),
             ImportColumn::make('gender_id')
-                ->numeric()
-                ->rules(['integer']),
+                ->numeric(),
             ImportColumn::make('children')
                 ->requiredMapping()
-                ->numeric()
-                ->rules(['required', 'integer']),
+                ->numeric(),
             ImportColumn::make('dpi')
                 ->requiredMapping()
-                ->numeric()
-                ->rules(['required', 'integer']),
+                ->numeric(),
             ImportColumn::make('nit')
                 ->requiredMapping()
-                ->numeric()
-                ->rules(['required', 'integer']),
+                ->numeric(),
             ImportColumn::make('email')
                 ->requiredMapping()
-                ->rules(['required', 'email', 'max:100'])
                 ->fillRecordUsing(function (Employee $record, string $state): void {
                     $record->email = strtolower($state);
                 }),
             ImportColumn::make('photo')
-                ->requiredMapping()
-                ->rules(['required', 'max:191']),
+                ->requiredMapping(),
             ImportColumn::make('nationality_id')
                 ->requiredMapping()
-                ->numeric()
-                ->rules(['required', 'integer']),
+                ->numeric(),
             ImportColumn::make('status_id')
-                ->numeric()
-                ->rules(['integer']),
+                ->numeric(),
             ImportColumn::make('id_payroll')
-                ->numeric()
-                ->rules(['integer']),
+                ->numeric(),
         ];
     }
 
