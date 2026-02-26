4a2a741de3c39f5592de151069bf14391a54e1b5
LinenBen17 <aesquitecastilo@gmail.com>
2026-02-26 13:06:18 -0600

    Modificacion ResourceUser para ingreso de Username y Mensajes de validacion en el formulario

--- app/Filament/Auth/CustomLogin.php
+++ app/Filament/Auth/CustomLogin.php
@@ -5,6 +5,7 @@ namespace App\Filament\Auth;
 use Filament\Forms\Components\Component;
 use Filament\Forms\Components\TextInput;
 use Filament\Pages\Auth\Login;
+use Illuminate\Validation\ValidationException;
 
 class CustomLogin extends Login
 {
@@ -31,6 +32,13 @@ class CustomLogin extends Login
             ->autocomplete()
             ->autofocus()
             ->extraInputAttributes(['tabindex' => 1]);
+    }
+
+    protected function throwFailureValidationException(): never
+    {
+        throw ValidationException::withMessages([
+            'data.login' => __('filament-panels::pages/auth/login.messages.failed'),
+        ]);
     }
 
     protected function getCredentialsFromFormData(array $data): array
